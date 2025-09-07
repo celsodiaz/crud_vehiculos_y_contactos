5de316d24a004128d47c56c70900248066bb7c35
celsodiaz <celso.acd.11@gmail.com>
2025-09-06 21:13:39 -0500

    Incorporacion del resource Collection para el retorno de servicio en json

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactoController;
+use App\Http\Controllers\VehiculoController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::resource('contactos', ContactoController::class);
+Route::resource('vehiculos', VehiculoController::class);
